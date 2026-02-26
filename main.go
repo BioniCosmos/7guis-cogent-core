06edea544c5911e65477b2e8c7abf87fd5a70a00
Daniel Ding <41363844+BioniCosmos@users.noreply.github.com>
2026-02-26 16:21:43 +0800

    Implement counter

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
-import "cogentcore.org/core/core"
+import (
+	"strconv"
+
+	"cogentcore.org/core/core"
+	"cogentcore.org/core/events"
+)
 
 func main() {
 	b := core.NewBody()
-	core.NewButton(b).SetText("Hello, World!")
+	counter(b)
 	b.RunMainWindow()
+}
+
+func counter(body *core.Body) {
+	i := 0
+	textField := core.NewTextField(body).SetText(strconv.Itoa(i))
+	textField.SetReadOnly(true)
+	core.NewButton(body).SetText("Count").OnClick(func(e events.Event) {
+		i++
+		textField.SetText(strconv.Itoa(i)).Update()
+	})
 }
